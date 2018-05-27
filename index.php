a27c811bfc92858f3899776e2552570cebb2f00d
Carlos <carloscorreia.87@gmail.com>
2018-05-27 15:50:25 +0100

    LOGIN e PESQUISA

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php 
 require_once("config.php");
-$root = new Usuario();
+/*$root = new Usuario();
 $root->loadById(1);
-echo $root;
-//$usuarios = 
+echo $root;*/
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//pesquisa
+//$search = Usuario::search("Carlos");
+//echo json_encode($search);
+
+//Login
+$usuario = new Usuario();
+$usuario->login("Carlos", "Teste123456789");
+echo $usuario;
 ?>
